fd47a0eb2212a05b3ec815f73b9cec9eae3988e8
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-11-25 15:51:41 +0100

    screen object model is added

--- test/specs/android/android-real-app.spec.js
+++ test/specs/android/android-real-app.spec.js
@@ -1,3 +1,5 @@
+import AddNoteScreen from '../../screenobjects/android/add-note.screen.js'
+
 describe('Android Real App Test : Add Notes', () => {
   it('Add a note', async () => {
     const title = 'My new note title'
@@ -14,5 +16,21 @@ describe('Android Real App Test : Add Notes', () => {
     await driver.back()
     await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_btn"]')).toBeDisplayed()
     await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/view_note"]')).toHaveText(body)
+  })
+
+  it.only('Add a note with Screen Object Model', async () => {
+    const title = 'My new note title'
+    const body = 'My new note\nBody'
+
+    await AddNoteScreen.skipButton.click()
+    await expect(AddNoteScreen.addNoteText).toBeDisplayed()
+    await AddNoteScreen.addNoteText.click()
+    await AddNoteScreen.textOption.click()
+    await expect(AddNoteScreen.textEditing).toBeDisplayed()
+    await AddNoteScreen.noteHeader.setValue(title)
+    await AddNoteScreen.noteBody.setValue(body)
+    await AddNoteScreen.saveNote()
+    await expect(AddNoteScreen.editButton).toBeDisplayed()
+    await expect(AddNoteScreen.viewNote).toHaveText(body)
   })
 })
